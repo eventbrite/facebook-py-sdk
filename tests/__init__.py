33e4b4e0ee4602c5dbfa322d4ce184e35354d542
Zeta <zetahernandez@gmail.com>
2016-09-11 01:24:21 -0300

    fix test python 2.6

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,23 @@
 import difflib
 import pprint
 from unittest import TestCase as UnitTestCase
-from unittest.util import safe_repr
 
 from facebook_sdk.request import FacebookRequest, FacebookBatchRequest
+
+
+def safe_repr(obj, short=False):
+    """
+    Helper class to provide backport support for `assertIn` and `assertIsInstance`
+    for python 2.6
+    """
+    MAX_LENGTH = 80
+    try:
+        result = repr(obj)
+    except Exception:
+        result = object.__repr__(obj)
+    if not short or len(result) < MAX_LENGTH:
+        return result
+    return result[:MAX_LENGTH] + ' [truncated]...'
 
 
 class TestCase(UnitTestCase):
