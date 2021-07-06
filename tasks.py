e9cfb7c52dc2c85f6ac3bab23c210919debc0336
abonilla-eb <80284934+abonilla-eb@users.noreply.github.com>
2021-07-06 15:56:51 -0300

    [MINOR][EB-166720] Setup deploy to CodeArtifact with CircleCI (#28)
    * configuration of circle ci
    
    * adding configuration on configure release parameters
    
    * Add correct fingerprint to circleci config
    
    Co-authored-by: vgatica-eb <vgatica@eventbrite.com>

--- tasks.py
+++ tasks.py
@@ -9,4 +9,6 @@ from invoke_release.tasks import *  # noqa: F403
 configure_release_parameters(  # noqa: F405
     module_name='facebook_sdk',
     display_name='FacebookSDK',
+    use_pull_request=True,
+    use_tag=False,
 )
